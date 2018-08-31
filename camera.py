976a364089e07dcec60da12dec42ff51aca4610d
chriskuan <chris@unnotech.com>
2018-08-31 14:32:14 +0800

    add chain and camera

---
+++ camera.py
@@ -0,0 +1,15 @@
+from pprint import pformat
+from celery.events.snapshot import Polaroid
+
+
+class Camera(Polaroid):
+    clear_after = True  # clear after flush (incl, state.event_count).
+
+    def on_shutter(self, state):
+        if not state.event_count:
+            # No new events since last snapshot.
+            return
+        print('Workers: {0}'.format(pformat(state.workers, indent=4)))
+        print('Tasks: {0}'.format(pformat(state.tasks, indent=4)))
+        print('Total: {0.event_count} events, {0.task_count} tasks'.format(
+              state))
--- vege1.py
+++ vege1.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 from celery import Celery
 from celery.schedules import crontab
+from celery import chain
 
 app = Celery('beat1')  # 建立一個Celery的instance
 
@@ -8,13 +9,16 @@ app = Celery('beat1')  # 建立一個Celery的instance
 # celery的相關參數設定
 class Config:
     broker_url = 'amqp://celeryman:pass1234@localhost:5672/celery_vhost'
-    result_backend = 'redis://localhost:6379/'
+    # result_backend = 'redis://localhost:6379/2'
+    result_backend = 'rpc://'
+    task_ignore_result = True
+    # task_store_errors_even_if_ignored = True
     timezone = 'Asia/Taipei'
     beat_schedule = {
         # 每 10 秒發送這個 hello 這個 task
         'add-every-10-seconds': {
             'task': 'vege1.hello',
-            'schedule': 10.0,
+            'schedule': 100.0,
             'args': ['my friend']
         },
         # 每分鐘 0 秒時發送 add 這個 task
@@ -35,9 +39,26 @@ def hello(name='world'):
     return f'hello {name}'
 
 
-@app.task
+@app.task(task_store_errors_even_if_ignored=True)
 def add(x, y):
-    return x + y
+    ans = x + y
+    if ans % 2:
+        return ans
+    else:
+        raise Exception
+
+
+@app.task
+def test(x):
+    out = 'start:'
+    out += x
+    print(out)
+    return out
+
+
+@app.task
+def run_chain(num):
+    chain(add.s(2, num), add.s(7), add.s(10)).apply_async()
 
 
 if __name__ == '__main__':
